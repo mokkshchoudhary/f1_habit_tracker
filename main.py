2c2ab5383d2653579bc6a4c5ec16e2fa432f7c85
Moksh Choudhary <mokkshchoudhary@gmail.com>
2025-12-29 14:36:00 +0000

    added creating habit feature and getting all habits feature.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
+from typing import Annotated
 import json
 
 app = FastAPI()
 
 class Habits(BaseModel):
-    id : int
-    name : str = Field(..., min_length = 3)
+    id : Annotated[str, Field(...,description = 'The id of the habit', examples = ['001'])]
+    name : Annotated[str, Field (..., description = 'Name of Habit')]
     description : str | None = None
 
 def load_data():
@@ -27,10 +29,22 @@ def home():
     return {'Title':'F1 based habit tracker'}
 
 @app.post('/create_habit')
-def CreateHabit(habits: Habits):
-    return habits
-
-@app.get('all_habits')
-def all_habits(habits:Habits):
+def CreateHabit(habit: Habits):
     data = load_data()
-    return data
+
+    if habit.id in data:
+        raise HTTPException (status_code = 400, description = 'Habit exist/ Bad request')
+    
+    data[habit.id] = habit.model_dump(exclude = ['id'])
+    
+    save_data(data)
+
+    return JSONResponse(status_code = 201, content = {'message':'Habit Created Successfully'})
+
+@app.get('/get_habit/{habit_id}')
+def view_habit(habit_id : str = Path (...,description = "ID of the habit in our Database", example ='1,2,3')):
+    data = load_data()
+
+    if habit_id in data:
+        return data[habit_id]
+    raise HTTPException(status_code = 404,detail = 'Invalid ID/page request')
